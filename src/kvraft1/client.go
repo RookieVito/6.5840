95f9535f17322c15688c6c0c10fa9e49897c16c7
Vito <yangyuxin20010216@163.com>
2026-03-03 16:10:16 +0000

    重构了lab4A，修改唯一id为自增

--- src/kvraft1/client.go
+++ src/kvraft1/client.go
@@ -1,16 +1,17 @@
 package kvraft
 
 import (
-	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
-)
+	"time"
 
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
+)
 
 type Clerk struct {
 	clnt    *tester.Clnt
 	servers []string
-	leader int // last successful leader (index into servers[])
+	leader  int // last successful leader (index into servers[])
 	// You can add to this struct.
 }
 
@@ -35,9 +36,30 @@ func (ck *Clerk) Leader() int {
 // must match the declared types of the RPC handler function's
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
-
-	// You will have to modify this function.
-	return "", 0, ""
+	args := rpc.GetArgs{Key: key}
+	for {
+		reply := rpc.GetReply{}
+		ok := ck.clnt.Call(ck.servers[ck.leader], "KVServer.Get", &args, &reply)
+		if ok {
+			if reply.Err == rpc.ErrWrongLeader {
+				ck.leader = (ck.leader + 1) % len(ck.servers)
+				continue
+			} else {
+				if reply.Err == rpc.ErrNoKey {
+					// 没有该键值对
+					return "", 0, rpc.ErrNoKey
+				}
+				if reply.Err == rpc.OK {
+					// Get成功，正常返回
+					return reply.Value, reply.Version, reply.Err
+				}
+			}
+		} else {
+			// 当前leader的网络不可靠问题，可能已经不是leader了，换其他的服务器试试
+			ck.leader = (ck.leader + 1) % len(ck.servers)
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
 
 // Put updates key with value only if the version in the
@@ -58,6 +80,46 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // must match the declared types of the RPC handler function's
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
-	// You will have to modify this function.
-	return ""
+	args := rpc.PutArgs{
+		Key:     key,
+		Value:   value,
+		Version: version,
+	}
+
+	isFirst := true
+	for {
+		reply := rpc.PutReply{}
+		ok := ck.clnt.Call(ck.servers[ck.leader], "KVServer.Put", &args, &reply)
+		if ok {
+			if reply.Err == rpc.ErrWrongLeader {
+				ck.leader = (ck.leader + 1) % len(ck.servers)
+				continue
+			} else {
+				if reply.Err == rpc.ErrVersion && isFirst {
+					return rpc.ErrVersion
+				}
+
+				if reply.Err == rpc.OK {
+					return rpc.OK
+				}
+
+				if reply.Err == rpc.ErrVersion && !isFirst {
+					//如果是重发，errmaybe
+					return rpc.ErrMaybe
+				}
+
+				if reply.Err == rpc.ErrNoKey {
+					return rpc.ErrNoKey
+				}
+
+				if reply.Err == rpc.ErrMaybe {
+					return rpc.ErrMaybe
+				}
+			}
+		} else {
+			isFirst = false
+			ck.leader = (ck.leader + 1) % len(ck.servers)
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
--- src/kvraft1/rsm/rsm.go
+++ src/kvraft1/rsm/rsm.go
@@ -1,22 +1,22 @@
 package rsm
 
 import (
+	"fmt"
 	"sync"
+	"time"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/raft1"
+	raft "6.5840/raft1"
 	"6.5840/raftapi"
-	"6.5840/tester1"
-
+	tester "6.5840/tester1"
 )
 
 type Op struct {
-	// Your definitions here.
-	// Field names must start with capital letters,
-	// otherwise RPC will break.
+	Me  int
+	Id  uint64 // 唯一id
+	Req any
 }
-
 
 // A server (i.e., ../server.go) that wants to replicate itself calls
 // MakeRSM and must implement the StateMachine interface.  This
@@ -38,6 +38,10 @@ type RSM struct {
 	maxraftstate int // snapshot if log grows this big
 	sm           StateMachine
 	// Your definitions here.
+	nextId      uint64 // 唯一自增Id，用于唯一标识每个Op
+	pending     map[uint64]chan struct{}
+	lastApplied int
+	results     map[uint64]any
 }
 
 // servers[] contains the ports of the set of
@@ -61,10 +65,17 @@ func MakeRSM(servers []*labrpc.ClientEnd, me int, persister *tester.Persister, m
 		maxraftstate: maxraftstate,
 		applyCh:      make(chan raftapi.ApplyMsg),
 		sm:           sm,
+		nextId:       0,
+		pending:      make(map[uint64]chan struct{}),
+		results:      make(map[uint64]any),
+		lastApplied:  0,
 	}
 	if !tester.UseRaftStateMachine {
 		rsm.rf = raft.Make(servers, me, persister, rsm.applyCh)
 	}
+
+	go rsm.reader()
+
 	return rsm
 }
 
@@ -72,16 +83,130 @@ func (rsm *RSM) Raft() raftapi.Raft {
 	return rsm.rf
 }
 
+func (rsm *RSM) reader() {
+	for msg := range rsm.applyCh {
+		err_msg := ""
+		if msg.SnapshotValid {
+			// install snapshot
+			err_msg = rsm.ingestSnap(msg.Snapshot, msg.SnapshotIndex)
+		} else if msg.CommandValid {
+			if msg.CommandIndex != rsm.lastApplied+1 {
+				// 判断是不是应该apply的log entry, cmd执行的顺序应该与log的记录顺序一致
+				err_msg = "rsm apply out of order"
+			}
+			rsm.lastApplied = msg.CommandIndex
+
+			result := rsm.sm.DoOp(msg.Command.(Op).Req)
+
+			// 1.判断是否需要快照
+			if rsm.maxraftstate != -1 && float32(rsm.rf.PersistBytes()) > float32(rsm.maxraftstate)*0.9 {
+				// 创建快照
+				snapshot := rsm.sm.Snapshot() // 获取kvServer的snapshot
+				rsm.rf.Snapshot(msg.CommandIndex, snapshot)
+			} else {
+				// Ignore other types of ApplyMsg
+			}
+
+			// 2.如果是自己submit的cmd，需要自己唤醒需要的submit线程
+			if msg.Command.(Op).Me != rsm.me {
+				// 如果submitServer不是自己，说明自己不是leader，仅执行
+				continue
+			}
+
+			rsm.mu.Lock()
+			if ch, ok := rsm.pending[msg.Command.(Op).Id]; ok {
+				rsm.results[msg.Command.(Op).Id] = result
+				rsm.mu.Unlock()
+				ch <- struct{}{}
+				continue
+			}
+			rsm.mu.Unlock()
+
+		} else {
+			// Ignore other types of ApplyMsg.
+		}
+		if err_msg != "" {
+			fmt.Println(err_msg)
+		}
+	}
+}
 
 // Submit a command to Raft, and wait for it to be committed.  It
 // should return ErrWrongLeader if client should find new leader and
 // try again.
 func (rsm *RSM) Submit(req any) (rpc.Err, any) {
 
-	// Submit creates an Op structure to run a command through Raft;
-	// for example: op := Op{Me: rsm.me, Id: id, Req: req}, where req
-	// is the argument to Submit and id is a unique id for the op.
+	rsm.mu.Lock()
+	id := rsm.nextId
+	op := Op{Me: rsm.me, Id: id, Req: req}
+	ch := make(chan struct{})
+	rsm.pending[rsm.nextId] = ch
+	rsm.nextId++
+	rsm.mu.Unlock()
 
-	// your code here
-	return rpc.ErrWrongLeader, nil // i'm dead, try another server.
+	_, submitTerm, submitIsLeader := rsm.Raft().Start(op)
+
+	if !submitIsLeader {
+		return rpc.ErrWrongLeader, nil
+	}
+
+	start := time.Now()
+	for {
+		// 判断是否term没变
+		// 判断ch是否有通知
+		// 如果超时10秒，删除map中id对应的元素
+
+		curTerm, _ := rsm.Raft().GetState()
+		rsm.mu.Lock()
+		if time.Since(start) >= 10*time.Second || curTerm != submitTerm {
+			reply, ok := rsm.results[id]
+			delete(rsm.results, id)
+			if ch, ok := rsm.pending[id]; ok {
+				close(ch)
+				delete(rsm.pending, id)
+			}
+
+			if ok {
+				rsm.mu.Unlock()
+				return rpc.OK, reply
+			} else {
+				rsm.mu.Unlock()
+				return rpc.ErrWrongLeader, nil
+			}
+		}
+		rsm.mu.Unlock()
+
+		select {
+		case _, ok := <-ch:
+			rsm.mu.Lock()
+			reply, ok := rsm.results[id]
+
+			delete(rsm.results, id)
+			if ch, ok := rsm.pending[id]; ok {
+				close(ch)
+				delete(rsm.pending, id)
+			}
+			rsm.mu.Unlock()
+			if ok {
+				return rpc.OK, reply
+			} else {
+				return rpc.ErrWrongLeader, nil
+			}
+
+		default:
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
+
+func (rsm *RSM) ingestSnap(snapshot []byte, index int) string {
+	rsm.mu.Lock()
+	defer rsm.mu.Unlock()
+
+	if snapshot == nil {
+		return "nil snapshot"
+	}
+	rsm.sm.Restore(snapshot)
+	rsm.lastApplied = index
+	return ""
 }
